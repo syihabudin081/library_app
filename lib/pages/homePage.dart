4261efafeaaf7bf3143f152a1e43cedca5470970
syihabudin081 <syihabupnyk@gmail.com>
2023-05-21 22:34:00 +0700

    login/register

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names, implementation_imports, camel_case_types
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
